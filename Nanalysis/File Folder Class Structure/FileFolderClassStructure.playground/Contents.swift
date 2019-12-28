94a5fdaa3ca571ce42ea6d2abb99561ee23cb6d6
Sophie Amin <aminsophie@gmail.com>
2019-12-28 16:16:51 -0700

    class skeletons for File and Folder, begin writing properties for File

---
+++ Nanalysis/File Folder Class Structure/FileFolderClassStructure.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class File {
+
+    var fileName: String
+    var content: String = ""
+    
+    init(fileName: String) {
+        self.fileName =  fileName
+    }
+}
+
+class Folder {
+    
+    
+}
