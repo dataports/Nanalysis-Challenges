23d43ff2011f6ab1607c632c2956f3dfc68c2db5
Sophie Amin <aminsophie@gmail.com>
2019-12-28 19:13:00 -0700

    started writing a main function for test, included init for Folder and filled out the class with properties for child files and folders to return

--- Nanalysis/File Folder Class Structure/FileFolderClassStructure.playground/Contents.swift
+++ Nanalysis/File Folder Class Structure/FileFolderClassStructure.playground/Contents.swift
@@ -8,9 +8,41 @@ class File {
     init(fileName: String) {
         self.fileName =  fileName
     }
+    
+    func getFileName() -> String {
+        return self.fileName
+    }
 }
 
+
 class Folder {
+    
+    var childFiles: [File]?
+    var childFolders: [Folder]?
+    var folderName: String
+    
+    init(folderName: String) {
+        self.folderName = folderName
+    }
+    
+    func childrenNames() -> [String]? {
+        //returns names of Files and Folders in childFiles and childFolders array, as well as their children
+        return nil
+    }
+    
+}
+
+
+func main() {
+    var grandparentFolder = Folder(folderName: "Grandparent")
+    var parentFolder = Folder(folderName: "Parent")
+    var childAFile = File(fileName: "Child A")
+    childAFile.content = "This is the content for Child A"
+    var childBFile = File(fileName: "Child B")
+    var childCFolder = Folder(folderName: "Child C")
+
+    
+    
     
     
 }
