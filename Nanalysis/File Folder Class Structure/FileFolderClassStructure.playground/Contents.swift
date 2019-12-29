c8e7519f69adb754ef44a456b05a982078a6287b
Sophie Amin <aminsophie@gmail.com>
2019-12-28 21:51:19 -0700

    Started writing methods to print information about the files name and hierarchy of the file system - realized I should probably make a subclass to represent a node to print the information about children

--- Nanalysis/File Folder Class Structure/FileFolderClassStructure.playground/Contents.swift
+++ Nanalysis/File Folder Class Structure/FileFolderClassStructure.playground/Contents.swift
@@ -12,17 +12,46 @@ class File {
     func getFileName() -> String {
         return self.fileName
     }
+    
+    func displayFileContent() {
+        print(content)
+    }
 }
 
+//TODO: make a node class that can be subclassed by files and folders to efficiently add/remove children and print out info
 
-class Folder {
+class Folder: CustomStringConvertible {
+    var description: String {
+        var text = "\value"
+        
+        if !childFolders.isEmpty || !childFiles.isEmpty {
+            text += " {" + childFolders.map { $0.description }.joined(separator: ", ") + childFiles.map { $0.description }.joined(separator: ", ") + "} "
+            
+        }
+        return text
+    }
     
-    var childFiles: [File]?
-    var childFolders: [Folder]?
+    
+    var childFiles: [File] = []
+    var childFolders: [Folder] = []
     var folderName: String
     
     init(folderName: String) {
         self.folderName = folderName
+    }
+    
+    //the add method parameters and name is different than in the example because I wanted to make it clear what type of child is being added for readability
+    func addChild(file: File?, folder: Folder?){
+        if file != nil && folder == nil {
+            childFiles.append(file!)
+        } else if file == nil && folder != nil {
+            childFolders.append(folder!)
+        } else if file != nil && folder != nil {
+            childFiles.append(file!)
+            childFolders.append(folder!)
+        } else {
+            return
+        }
     }
     
     func childrenNames() -> [String]? {
@@ -30,6 +59,9 @@ class Folder {
         return nil
     }
     
+    func displayHierarchyInfo() {
+        
+    }
 }
 
 
@@ -40,9 +72,18 @@ func main() {
     childAFile.content = "This is the content for Child A"
     var childBFile = File(fileName: "Child B")
     var childCFolder = Folder(folderName: "Child C")
-
+    
+    var grandChildFile = File(fileName: "GrandChild")
+    childCFolder.addChild(file: grandChildFile, folder: nil)
+    parentFolder.addChild(file: childAFile, folder: nil)
+    parentFolder.addChild(file: childBFile, folder: nil)
+    parentFolder.addChild(file: nil, folder: childCFolder)
+    grandparentFolder.addChild(file: nil, folder: parentFolder)
+    childAFile.displayFileContent()
     
     
     
     
 }
+
+main()
