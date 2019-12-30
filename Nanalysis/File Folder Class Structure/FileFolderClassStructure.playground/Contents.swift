ae271ec5847c441a0f3c7e54176dc1c77fb053cb
Sophie Amin <aminsophie@gmail.com>
2019-12-29 17:58:53 -0700

    added node class

--- Nanalysis/File Folder Class Structure/FileFolderClassStructure.playground/Contents.swift
+++ Nanalysis/File Folder Class Structure/FileFolderClassStructure.playground/Contents.swift
@@ -1,16 +1,37 @@
 import UIKit
 
-class File {
-
-    var fileName: String
-    var content: String = ""
-    
-    init(fileName: String) {
-        self.fileName =  fileName
+class Node: CustomStringConvertible {
+    var description: String {
+        // 3
+        var text = "\(nodeName)"
+        
+        // 4
+        if !children.isEmpty {
+            text += " {" + children.map { $0.description }.joined(separator: ", ") + "} "
+        }
+        return text
     }
     
-    func getFileName() -> String {
-        return self.fileName
+    var nodeName: String
+    var children: [Node] = []
+    weak var parent: Node?
+    
+    init(name: String) {
+        self.nodeName = name
+    }
+    
+    func add(child: Node) {
+        children.append(child)
+        child.parent = self
+    }
+}
+
+class File: Node {
+
+    var content: String = ""
+    
+    override init(name: String) {
+        super.init(name: name)
     }
     
     func displayFileContent() {
@@ -18,67 +39,29 @@ class File {
     }
 }
 
-//TODO: make a node class that can be subclassed by files and folders to efficiently add/remove children and print out info
-
-class Folder: CustomStringConvertible {
-    var description: String {
-        var text = "\value"
-        
-        if !childFolders.isEmpty || !childFiles.isEmpty {
-            text += " {" + childFolders.map { $0.description }.joined(separator: ", ") + childFiles.map { $0.description }.joined(separator: ", ") + "} "
-            
-        }
-        return text
+class Folder: Node {
+    
+    override init(name: String) {
+        super.init(name: name)
     }
     
-    
-    var childFiles: [File] = []
-    var childFolders: [Folder] = []
-    var folderName: String
-    
-    init(folderName: String) {
-        self.folderName = folderName
-    }
-    
-    //the add method parameters and name is different than in the example because I wanted to make it clear what type of child is being added for readability
-    func addChild(file: File?, folder: Folder?){
-        if file != nil && folder == nil {
-            childFiles.append(file!)
-        } else if file == nil && folder != nil {
-            childFolders.append(folder!)
-        } else if file != nil && folder != nil {
-            childFiles.append(file!)
-            childFolders.append(folder!)
-        } else {
-            return
-        }
-    }
-    
-    func childrenNames() -> [String]? {
-        //returns names of Files and Folders in childFiles and childFolders array, as well as their children
-        return nil
-    }
-    
-    func displayHierarchyInfo() {
-        
-    }
 }
 
 
 func main() {
-    var grandparentFolder = Folder(folderName: "Grandparent")
-    var parentFolder = Folder(folderName: "Parent")
-    var childAFile = File(fileName: "Child A")
+    let grandparentFolder = Folder(name: "Grandparent")
+    let parentFolder = Folder(name: "Parent")
+    let childAFile = File(name: "Child A")
     childAFile.content = "This is the content for Child A"
-    var childBFile = File(fileName: "Child B")
-    var childCFolder = Folder(folderName: "Child C")
+    let childBFile = File(name: "Child B")
+    let childCFolder = Folder(name: "Child C")
     
-    var grandChildFile = File(fileName: "GrandChild")
-    childCFolder.addChild(file: grandChildFile, folder: nil)
-    parentFolder.addChild(file: childAFile, folder: nil)
-    parentFolder.addChild(file: childBFile, folder: nil)
-    parentFolder.addChild(file: nil, folder: childCFolder)
-    grandparentFolder.addChild(file: nil, folder: parentFolder)
+    let grandChildFile = File(name: "GrandChild")
+    childCFolder.add(child: grandChildFile)
+    parentFolder.add(child: childAFile)
+    parentFolder.add(child: childBFile)
+    parentFolder.add(child: childCFolder)
+    grandparentFolder.add(child: parentFolder)
     childAFile.displayFileContent()
     
     
